a1cdb42f4ee77c3e50024923895a26951e791f22
Rafael <rafinha_fern@hotmail.com>
2022-03-23 01:20:04 -0300

    Home de escolha do filme pronto seguindo o diretório /

--- src/App/components/home/index.js
+++ src/App/components/home/index.js
@@ -1,7 +1,26 @@
+import imgtest from "../../../assets/images/imagetest.png";
+import "./styles.css"
 function Home() {
     return (
         <main>
-            <article className="posters-board"><img className="poster"></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article><article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+            <article className="posters-board"><img className="poster" src={imgtest}></img></article>
+
         </main>
     )
 }
