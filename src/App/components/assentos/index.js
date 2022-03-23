513f47eb2ae0cc7b556076fdfd9872edfb03cbc2
Rafael <rafinha_fern@hotmail.com>
2022-03-23 02:55:37 -0300

    Pagina assentos com apresentação dos lugares e descrição de cores prontas

---
+++ src/App/components/assentos/index.js
@@ -0,0 +1,53 @@
+import "./styles.css"
+function Assentos() {
+    return (
+        <main>
+            <section className="assentos">
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+                <div className="assento"><h3>1</h3></div>
+            </section>
+            <section className="details">
+            <div className="assento detail selecionado"><h3>Selecionado</h3></div>
+            <div className="assento detail disponivel"><h3>Disponível</h3></div>
+            <div className="assento detail indisponivel"><h3>Indisponível</h3></div>
+            </section>
+        </main>
+    )
+}
+export default Assentos;
--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Home from "./components/home";
 import Sessao from "./components/sessao";
+import Assentos from "./components/assentos";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sessao" element={<Sessao/>} />
+          <Route path="/assentos" element={<Assentos/>} />
         </Routes>
       </BrowserRouter>
     </>
